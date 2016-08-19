0b622cdb6381cf29071f74f84e8a9b5af12f03c4
Hanno Hecker <hanno.hecker@zalando.de>
2016-08-19 08:10:05 +0200

    go vet / golint, add Makefile

--- ldapdn_test.go
+++ ldapdn_test.go
@@ -1,30 +1,52 @@
-package ldapdn_test
+package ldapdn
 
 import (
 	"fmt"
 	"sort"
 	"testing"
-
-	"github.com/vetinari/go-ldapdn"
 )
 
 func TestDNString(t *testing.T) {
 	fmt.Printf("DNString: starting...\n")
-	dn, _ := ldapdn.New("OU=Sales+CN=J. Smith,DC=example,DC=net")
+	dn, _ := New("OU=Sales+CN=J. Smith,DC=example,DC=net")
 	strdn := dn.String()
 	if strdn != "ou=Sales+cn=J. Smith,dc=example,dc=net" {
 		t.Errorf("Failed to stringify: %v\n", strdn)
 	}
 	fmt.Printf("DNString: -> %v\n", strdn)
-	dn2, _ := ldapdn.New("CN=Lučić\\+Ma\\=><foo")
-	if _, err := ldapdn.New(dn2.String()); err != nil {
+	dn2, _ := New("CN=Lučić\\+Ma\\=><foo")
+	if _, err := New(dn2.String()); err != nil {
 		t.Errorf("Failed to parse stringified DN: %s", err)
+	}
+}
+
+func TestRDNString(t *testing.T) {
+	dn, _ := New("ou=Sales+cn=J. Smith")
+	strdn := dn.FirstRDN().String()
+	if strdn != "ou=Sales+cn=J. Smith" {
+		t.Errorf("Failed to stringify: %v\n", strdn)
+	}
+}
+
+func TestDNInvalidString(t *testing.T) {
+	dn, err := New("uid=foo,bar,dc=example,dc=org")
+	if err != nil {
+		t.Errorf("did not fail to parse invalid DN %s", dn)
+	}
+}
+
+func TestEscapeValue(t *testing.T) {
+	str := "test\atest"
+	res := escapeValue(str)
+	fmt.Printf("STR=%v\n", res)
+	if res != `test\07test` {
+		t.Errorf("did not escape \\a correctly")
 	}
 }
 
 func TestCanonicalDN(t *testing.T) {
 	dn := "ou=Sales+CN=J. Smith,  DC=example, DC=net"
-	cdn, _ := ldapdn.CanonicalDN(dn)
+	cdn, _ := CanonicalDN(dn)
 	if cdn != "ou=Sales+cn=J. Smith,dc=example,dc=net" {
 		t.Errorf("Canonical DN Failed: %s\n", cdn)
 	}
@@ -32,7 +54,7 @@ func TestCanonicalDN(t *testing.T) {
 
 func TestDNParent(t *testing.T) {
 	fmt.Printf("DN Parent: starting...\n")
-	dn, _ := ldapdn.New("OU=Sales+CN=J. Smith,DC=example,DC=net")
+	dn, _ := New("OU=Sales+CN=J. Smith,DC=example,DC=net")
 	parent := dn.Parent()
 	if dn.String() != "ou=Sales+cn=J. Smith,dc=example,dc=net" {
 		t.Errorf("original dn modified -> %s\n", dn)
@@ -41,13 +63,18 @@ func TestDNParent(t *testing.T) {
 		t.Errorf("wrong parent -> %s\n", parent)
 	}
 	fmt.Printf("DN Parent: %s -> %s\n", dn, parent)
+	dn = &DN{}
+	parent = dn.Parent()
+	if parent.String() != dn.String() {
+		t.Errorf("Parent() is not equal DN with empty DN")
+	}
 }
 
 func TestDNMove(t *testing.T) {
 	fmt.Printf("DN Rename and Move: starting...\n")
-	dn, _ := ldapdn.New("OU=Sales+CN=J. Smith,DC=example,DC=net")
-	base, _ := ldapdn.New("OU=People,DC=example,DC=net")
-	rdn, _ := ldapdn.New("cn=J. Smith")
+	dn, _ := New("OU=Sales+CN=J. Smith,DC=example,DC=net")
+	base, _ := New("OU=People,DC=example,DC=net")
+	rdn, _ := New("cn=J. Smith")
 	dn.Move(base)
 	if dn.String() != "ou=Sales+cn=J. Smith,ou=People,dc=example,dc=net" {
 		t.Errorf("Failed to move: %s\n", dn)
@@ -60,8 +87,8 @@ func TestDNMove(t *testing.T) {
 }
 
 func TestDNEqual(t *testing.T) {
-	dn1, _ := ldapdn.New("OU=people,DC=example,DC=org")
-	dn2, _ := ldapdn.New("ou=People,dc=Example,dc=ORG")
+	dn1, _ := New("OU=people,DC=example,DC=org")
+	dn2, _ := New("ou=People,dc=Example,dc=ORG")
 	dn1.CaseFold = true
 	dn2.CaseFold = true
 	if !dn1.Equal(dn2) {
@@ -75,7 +102,7 @@ func TestDNEqual(t *testing.T) {
 }
 
 func TestDNSort(t *testing.T) {
-	var dns []*ldapdn.DN
+	var dns []*DN
 	dnStrings := []string{
 		"ou=people,dc=example,dc=org",
 		"uid=another,ou=people,dc=example,dc=org",
@@ -87,10 +114,10 @@ func TestDNSort(t *testing.T) {
 	}
 
 	for _, s := range dnStrings {
-		dn, _ := ldapdn.New(s)
+		dn, _ := New(s)
 		dns = append(dns, dn)
 	}
-	sort.Sort(ldapdn.DNs(dns))
+	sort.Sort(DNS(dns))
 	for _, dn := range dns {
 		fmt.Printf("DN: %s\n", dn.String())
 	}
@@ -103,8 +130,8 @@ func TestDNSort(t *testing.T) {
 }
 
 func TestDNClone(t *testing.T) {
-	dn, _ := ldapdn.New("uid=someone,ou=people,dc=example,dc=org")
-	newrdn, _ := ldapdn.New("uid=foobar")
+	dn, _ := New("uid=someone,ou=people,dc=example,dc=org")
+	newrdn, _ := New("uid=foobar")
 	clone := dn.Clone()
 	dn.Rename(newrdn.FirstRDN())
 	fmt.Printf("RENAMED=%s, CLONE=%s\n", dn, clone)
@@ -114,11 +141,31 @@ func TestDNClone(t *testing.T) {
 }
 
 func TestDNPretty(t *testing.T) {
-	dn, _ := ldapdn.New("cn=group,ou=some,ou=apps,dc=example,dc=org")
-	base, _ := ldapdn.New("dc=example,dc=org")
+	dn, _ := New("cn=group,ou=some,ou=apps,dc=example,dc=org")
+	base, _ := New("dc=example,dc=org")
 	p := dn.Pretty(base, "/", nil)
-	fmt.Printf("Pretty: %s\n", p)
 	if p != "Apps/Some/Group" {
 		t.Errorf("Petty failed: %s\n", p)
+	}
+	p = dn.Pretty(base, "", nil)
+	if p != "Apps/Some/Group" {
+		t.Errorf("Petty failed: %s\n", p)
+	}
+
+}
+
+func TestIsSubordinate(t *testing.T) {
+	dn, _ := New("cn=group,ou=some,ou=apps,dc=example,dc=org")
+	if dn.IsSubordinate(nil) {
+		t.Errorf("did not fail IsSubordinate() on nil parent")
+	}
+	base, _ := New("dc=example,dc=org")
+	if !dn.IsSubordinate(base) {
+		t.Errorf("fail IsSubordinate() on parent")
+	}
+
+	notBase, _ := New("dc=example,dc=com")
+	if dn.IsSubordinate(notBase) {
+		t.Errorf("did not fail IsSubordinate() on non parent")
 	}
 }
